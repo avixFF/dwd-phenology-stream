70dc4aa30ea27fde660470b97c9a9b260996240b
Alexey Ivanov <lexuzieel@gmail.com>
2020-07-10 00:46:33 +0200

    Add database config

--- database/__init__.py
+++ database/__init__.py
@@ -3,7 +3,11 @@ from peewee import MySQLDatabase
 from playhouse.migrate import MySQLMigrator
 
 connection = MySQLDatabase(
-    database=os.getenv('MYSQL_DATABASE')
+    database=os.getenv('MYSQL_DATABASE'),
+    host=os.getenv('MYSQL_HOST', '127.0.0.1'),
+    port=int(os.getenv('MYSQL_PORT', 3306)),
+    user=os.getenv('MYSQL_USERNAME'),
+    password=os.getenv('MYSQL_PASSWORD'),
 )
 
 migrator = MySQLMigrator(connection)
