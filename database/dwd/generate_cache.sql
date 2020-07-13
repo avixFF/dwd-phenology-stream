a5f40d9a2db73eb13fcb185974c6ecb4e8d6ff50
Alexey Ivanov <lexuzieel@gmail.com>
2020-07-13 21:53:05 +0200

    Implement dwd cache generation
    This cache is similar to materialized views
    and is used to speed up reading times

---
+++ database/dwd/generate_cache.sql
@@ -0,0 +1,62 @@
+drop table if exists dwd_records_cache_fruits;
+create table dwd_records_cache_fruits
+(
+    key `weight` (`weight`),
+    key `lat` (`lat`),
+    key `lon` (`lon`),
+    key `state` (`state`),
+    key `date` (`date`)
+)
+select avg(dwd_phases.weight) as "weight",
+       dwd_stations.longitude as "lon",
+       dwd_stations.latitude  as "lat",
+       dwd_stations.state     as "state",
+       dwd_records.date       as "date"
+from dwd_records
+         join dwd_plants on dwd_plants.id = dwd_records.object_id
+         join dwd_phases on dwd_phases.id = dwd_records.phase_id
+         join dwd_stations on dwd_stations.id = dwd_records.station_id
+where kind = 'fruit'
+group by date, lon, lat, state;
+
+drop table if exists dwd_records_cache_crops;
+create table dwd_records_cache_crops
+(
+    key `weight` (`weight`),
+    key `lat` (`lat`),
+    key `lon` (`lon`),
+    key `state` (`state`),
+    key `date` (`date`)
+)
+select avg(dwd_phases.weight) as "weight",
+       dwd_stations.longitude as "lon",
+       dwd_stations.latitude  as "lat",
+       dwd_stations.state     as "state",
+       dwd_records.date       as "date"
+from dwd_records
+         join dwd_plants on dwd_plants.id = dwd_records.object_id
+         join dwd_phases on dwd_phases.id = dwd_records.phase_id
+         join dwd_stations on dwd_stations.id = dwd_records.station_id
+where kind = 'crop'
+group by date, lon, lat, state;
+
+drop table if exists dwd_records_cache_wild;
+create table dwd_records_cache_wild
+(
+    key `weight` (`weight`),
+    key `lat` (`lat`),
+    key `lon` (`lon`),
+    key `state` (`state`),
+    key `date` (`date`)
+)
+select avg(dwd_phases.weight) as "weight",
+       dwd_stations.longitude as "lon",
+       dwd_stations.latitude  as "lat",
+       dwd_stations.state     as "state",
+       dwd_records.date       as "date"
+from dwd_records
+         join dwd_plants on dwd_plants.id = dwd_records.object_id
+         join dwd_phases on dwd_phases.id = dwd_records.phase_id
+         join dwd_stations on dwd_stations.id = dwd_records.station_id
+where kind = 'wild'
+group by date, lon, lat, state;
--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# import .env
+if [ -f .env ]; then
+    export $(egrep -v '^#' .env | xargs)
+fi
+
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 pipenv=/usr/local/bin/pipenv
 log_file=run.log
@@ -10,3 +15,13 @@ echo "Ran on $(date)" > $log_file
 
 $pipenv install >> $log_file
 $pipenv run python . >> $log_file
+
+echo "Generating cache..." >> $log_file
+
+if [ -z $MYSQL_PASSWORD ]; then
+    mysql -u $MYSQL_USERNAME $MYSQL_DATABASE < database/dwd/generate_cache.sql
+else
+    mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD $MYSQL_DATABASE < database/dwd/generate_cache.sql
+fi
+
+echo "Done"
