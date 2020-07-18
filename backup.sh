9753ed1653f33d44420de665ecab31e26e6cbe66
Alexey Ivanov <lexuzieel@gmail.com>
2020-07-18 05:13:54 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -7,7 +7,12 @@ fi
 
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 timestamp=$(date +%Y%m%d%H%M%S)
+dir="$script_dir/database/dumps"
+num_backups=5
 
-mkdir -p $script_dir/database/dumps
+mkdir -p $dir
 
-mysqldump -u $MYSQL_BACKUP_USERNAME -p$MYSQL_BACKUP_PASSWORD $MYSQL_DATABASE > $script_dir/database/dumps/$timestamp.sql
+mysqldump -u $MYSQL_BACKUP_USERNAME -p$MYSQL_BACKUP_PASSWORD $MYSQL_DATABASE > $dir/$timestamp.sql
+
+# Remove old backups, leave $num_backups most recent
+ls -1tr $dir/* | head -n -$num_backups | xargs --no-run-if-empty rm
