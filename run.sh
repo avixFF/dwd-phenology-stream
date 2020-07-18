b59bb287daa51c01816a7f9833d1ae230b724b08
Alexey Ivanov <lexuzieel@gmail.com>
2020-07-18 04:52:54 +0200

    Update run script

--- run.sh
+++ run.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
+script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+pipenv=/usr/local/bin/pipenv
+
+cd "$script_dir"
+
 # import .env
 if [ -f .env ]; then
     export $(egrep -v '^#' .env | xargs)
 fi
 
-script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-pipenv=/usr/local/bin/pipenv
-log_file=run.log
+echo "Ran on $(date)"
 
-cd "$script_dir"
+$pipenv install
+$pipenv run python .
 
-echo "Ran on $(date)" > $log_file
-
-$pipenv install >> $log_file
-$pipenv run python . >> $log_file
-
-echo "Generating cache..." >> $log_file
+echo "Generating cache..."
 
 if [ -z $MYSQL_PASSWORD ]; then
     mysql -u $MYSQL_USERNAME $MYSQL_DATABASE < "$script_dir/database/dwd/generate_cache.sql"
@@ -24,4 +23,4 @@ else
     mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD $MYSQL_DATABASE < "$script_dir/database/dwd/generate_cache.sql"
 fi
 
-echo "Done" >> $log_file
+echo "Done"
