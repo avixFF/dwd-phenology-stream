b4f1fb3a8bbf89ec33d2eeba233cb2db5a0bb64e
Alexey Ivanov <lexuzieel@gmail.com>
2020-07-25 16:39:06 +0200

    Update dwd cache generation
    Change state to station name to avoid duplicates

--- database/dwd/generate_cache.sql
+++ database/dwd/generate_cache.sql
@@ -4,20 +4,20 @@ create table dwd_records_cache_fruits
     key `weight` (`weight`),
     key `lat` (`lat`),
     key `lon` (`lon`),
-    key `state` (`state`),
+    key `station` (`station`),
     key `date` (`date`)
 )
 select avg(dwd_phases.weight) as "weight",
        dwd_stations.longitude as "lon",
        dwd_stations.latitude  as "lat",
-       dwd_stations.state     as "state",
+       dwd_stations.name      as "station",
        dwd_records.date       as "date"
 from dwd_records
          join dwd_plants on dwd_plants.id = dwd_records.object_id
          join dwd_phases on dwd_phases.id = dwd_records.phase_id
          join dwd_stations on dwd_stations.id = dwd_records.station_id
 where kind = 'fruit'
-group by date, lon, lat, state;
+group by date, lon, lat, station;
 
 drop table if exists dwd_records_cache_crops;
 create table dwd_records_cache_crops
@@ -25,20 +25,20 @@ create table dwd_records_cache_crops
     key `weight` (`weight`),
     key `lat` (`lat`),
     key `lon` (`lon`),
-    key `state` (`state`),
+    key `station` (`station`),
     key `date` (`date`)
 )
 select avg(dwd_phases.weight) as "weight",
        dwd_stations.longitude as "lon",
        dwd_stations.latitude  as "lat",
-       dwd_stations.state     as "state",
+       dwd_stations.name      as "station",
        dwd_records.date       as "date"
 from dwd_records
          join dwd_plants on dwd_plants.id = dwd_records.object_id
          join dwd_phases on dwd_phases.id = dwd_records.phase_id
          join dwd_stations on dwd_stations.id = dwd_records.station_id
 where kind = 'crop'
-group by date, lon, lat, state;
+group by date, lon, lat, station;
 
 drop table if exists dwd_records_cache_wild;
 create table dwd_records_cache_wild
@@ -46,17 +46,17 @@ create table dwd_records_cache_wild
     key `weight` (`weight`),
     key `lat` (`lat`),
     key `lon` (`lon`),
-    key `state` (`state`),
+    key `station` (`station`),
     key `date` (`date`)
 )
 select avg(dwd_phases.weight) as "weight",
        dwd_stations.longitude as "lon",
        dwd_stations.latitude  as "lat",
-       dwd_stations.state     as "state",
+       dwd_stations.name      as "station",
        dwd_records.date       as "date"
 from dwd_records
          join dwd_plants on dwd_plants.id = dwd_records.object_id
          join dwd_phases on dwd_phases.id = dwd_records.phase_id
          join dwd_stations on dwd_stations.id = dwd_records.station_id
 where kind = 'wild'
-group by date, lon, lat, state;
+group by date, lon, lat, station;
